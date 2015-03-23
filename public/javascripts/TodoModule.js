bf419f956b506d5da3bb47ae54bd44ab7c4975fb
Wenson Tsai <wensontsai@gmail.com>
2015-03-22 21:50:19 -0400

    frst

---
+++ public/javascripts/TodoModule.js
@@ -0,0 +1,3 @@
+angular.module('TodoModule', ['ui.bootstrap']);
+
+// this is just for angular-bootstrap to add datepicker.  no other purpose //
---
+++ server.js
@@ -0,0 +1,101 @@
+//////////////////////
+// Module Dependencies
+//////////////////////
+var http = require('http')
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+///////// db connect  //////////////////
+var Mongoose = require('mongoose');
+var db = Mongoose.createConnection('mongodb://localhost/last_todo_ever',
+        function(err){
+            if(err){
+                console.log('connection error', err);
+            } else {
+                console.log('connection successfull');
+            }
+        });
+
+var TodoSchema = require('./models/Todo.js').TodoSchema;
+var Todo = db.model('todos', TodoSchema);
+
+
+/////////////////////
+// view engine setup
+// all environments
+/////////////////////
+app.set('port', process.env.PORT || 3030);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+// app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/////////////////////
+// AngularJS  ROUTING
+/////////////////////
+app.get('/', routes.index(Todo));
+app.get('/todos.json', routes.get(Todo));
+app.put('/todo/:id/json', routes.update(Todo));
+app.post('/todo.json', routes.addTodo(Todo));
+
+// var user = require('.routes/user');
+// app.get('/users', user.list);
+
+
+
+
+
+////////   catch 404 and forward to error handler  ////////
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+//////////////////
+/// error handlers
+//////////////////
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+//////////////////////////
+// server + listening port
+//////////////////////////
+http.createServer(app).listen(3030);
+console.log('Express server listening on port ' + app.get('port'));

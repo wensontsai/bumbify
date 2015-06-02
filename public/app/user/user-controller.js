77531daee5537c52b5f17a8742bd6ed319b3dc52
Wenson Tsai <wensontsai@gmail.com>
2015-06-02 14:30:08 -0400

    signup view working

--- public/app/user/user-controller.js
+++ public/app/user/user-controller.js
@@ -1,16 +1,23 @@
 routerApp
   .controller('UserCtrl',
-    function($rootScope, $scope, $http){
+    function($rootScope, $scope, $http, $location){
 
       $scope.signupData = {};
 
       // Signup - add new user
       $scope.signUp = function(){
+        // validations go here
+
+        // if all good, then hit up api
         $http.post('/api/signup', $scope.signupData).success(function(data){
             console.log(data);
+
+          // clear $scope.signupData
+          $scope.signupData = {};
+          // redirect to login page
+          $location.path('/login');
         });
 
-      console.log($scope.signupData);
       };
 
     });
